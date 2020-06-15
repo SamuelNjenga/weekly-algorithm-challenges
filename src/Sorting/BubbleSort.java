01068feb4c2e2b5f5c9abac0c60a1b5c239a0f67
Samuel Njenga <snjenga704@gmail.com>
2020-06-15 09:10:39 +0300

    Added BubbleSort.java

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {6, 4, 5, 3, 2, 1};
+        print(Arrays.toString(array));
+        for (int i = 0, size = array.length; i < size - 1; i++) {
+            for(int j = 0;j < size - 1 - i;j++){
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        print(Arrays.toString(array));
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
+}
