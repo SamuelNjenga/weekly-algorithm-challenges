99c8f4a2818cd971f8d74e42ac9df05e89a4f664
Samuel Njenga <snjenga704@gmail.com>
2020-08-13 14:18:17 +0300

    Added LinkedList directory and added its implementation

---
+++ src/LinkedList/MyLinkedList.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class MyLinkedList {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(6);
+        list.insert(12);
+        list.show();
+    }
+}
